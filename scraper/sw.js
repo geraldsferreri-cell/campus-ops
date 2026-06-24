80027e69192706a95b9f6ec6f82ab505b8bd3581
Gerald <geraldsferreri-cell@users.noreply.github.com>
2026-06-24 11:36:05 +0800

    添加PWA支持

---
+++ scraper/sw.js
@@ -0,0 +1,25 @@
+
+  const CACHE_NAME = 'campus-ops-v1';
+  const ASSETS = [
+    '/campus-ops/',
+    '/campus-ops/index.html'
+  ];
+
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+  });
+
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cached) => {
+        return cached || fetch(event.request).then((response) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
